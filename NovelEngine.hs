877a896c3d5783207566e2704ac107f56188cb76
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-08-11 19:47:56 +0900

    イベント雛形

--- NovelEngine.hs
+++ NovelEngine.hs
@@ -6,11 +6,35 @@ import Text.Parsec (parseTest)
 
 main = start novelGame
 
+--GUIのコントロール、及びイベント時のアクションを記述
 novelGame :: IO()
 novelGame = do
+    contents <- varCreate []
     str <- readFile "text.txt"
     title <- parseTest nString str
+    let orders = [Mes "" "はじまりはじまりー"]
     f <- frame [text := "ノベルゲームエンジン"]
-    p <- panel f []
-    set p [layout := column 1 [minsize (sz 100 100) $ label str] ]
-    set f [layout := widget p , clientSize := sz 800 600]
+    p <- panel f [on paint := paintScreen contents]
+    set p [on click := (\pt -> print "clicked")      -- 進行
+          ,on (charKey 'm') := playLoop $ sound "test.mp3" -- sound test
+          ,on clickRight := showMenu p contents  -- menu表示
+          ]
+    set f [layout := minsize (sz 800 600) $ widget p] where
+
+--スクリーンの描画
+paintScreen :: Var [Order] -> DC a -> Rect -> IO()
+paintScreen vContents dc viewArea = do
+    contents <- varGet vContents
+    set dc [brushColor := red , brushKind := BrushSolid]
+    mapM_ (drawPart dc) contents --描画対象それぞれを描画
+
+--オブジェクトごとの描画を記述
+drawPart dc order = case order of
+    _ -> circle dc (Point 400 300) radius [] where radius = 10
+
+--メニュー画面の描画を記述(ファイルIOを含む)
+showMenu p  contents pt = do
+    varUpdate contents (menuScreen:)
+    repaint p
+
+menuScreen = Mes "" ""
